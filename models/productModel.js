f3c9d185c011576a4f3e114fa5c77ae841ac0432
Sangam Chandra <122662832+Sangamchandra@users.noreply.github.com>
2023-06-27 00:23:32 +0530

    Add files via upload

---
+++ models/productModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        slug: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        contactno: {
+            type: Number,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: mongoose.ObjectId,
+            ref: "Category",
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        photo: {
+            data: Buffer,
+            contentType: String,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Products", productSchema);
